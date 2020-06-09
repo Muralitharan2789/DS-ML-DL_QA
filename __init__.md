adbe53d4eab71fa3dafb5c25e3b07fc17e2e0088
Muralitharan S <58542286+Muralitharan2789@users.noreply.github.com>
2020-06-09 14:12:26 +0530

    Update and rename __init__.py to __init__.md

---
+++ __init__.md
@@ -0,0 +1,6 @@
+### What is __init__.py in Python?
+
+- The __init__.py files are required to make Python treat the directories as containing packages; 
+
+- In the simplest case, __init__.py can just be an empty file, but it can also execute initialization code for the package or set the __all__ variable.
+
--- __init__.py
+++
@@ -1,5 +0,0 @@
-'''
-- The __init__.py files are required to make Python treat the directories as containing packages; 
-
-- In the simplest case, __init__.py can just be an empty file, but it can also execute initialization code for the package or set the __all__ variable. An example of the __init__.py file:
-'''
